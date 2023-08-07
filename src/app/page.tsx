74b2ebacda53be6527a54aea6abb982c498848a7
Rafael Pelle <rafapelle@gmail.com>
2023-08-07 02:31:37 -0300

    fix: index page responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,11 @@
 import { AnimatedLetter, ChatLoading, ChatMessage } from "@/components";
 import { ChatMessageProps } from "@/components/ChatMessage";
 import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 const chatMessages: ChatMessageProps[] = [
   { text: "Hi," },
   { text: "I'm Rafael Pelle!" },
-  { text: "You can ask me a question, or navigate through the menu." },
-  { text: "You can ask me a question, or navigate through the menu." },
-  { text: "You can ask me a question, or navigate through the menu." },
-  { text: "You can ask me a question, or navigate through the menu." },
-  { text: "You can ask me a question, or navigate through the menu." },
-  { text: "You can ask me a question, or navigate through the menu." },
   { text: "You can ask me a question, or navigate through the menu." },
 ];
 
@@ -23,6 +17,22 @@ export default function IndexPage() {
   const [isTyping, setIsTyping] = useState<boolean>(true);
 
   const messagesEndRef = useRef<null | HTMLDivElement>(null);
+
+  const firstName = useMemo(
+    () =>
+      "Rafael"
+        .split("")
+        .map((letter, index) => <AnimatedLetter key={index} letter={letter} />),
+    []
+  );
+
+  const lastName = useMemo(
+    () =>
+      "Pelle"
+        .split("")
+        .map((letter, index) => <AnimatedLetter key={index} letter={letter} />),
+    []
+  );
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -53,36 +63,31 @@ export default function IndexPage() {
   }, [contentIndex]);
 
   return (
-    <div className="flex flex-col justify-evenly lg:justify-between h-full p-3 max-w-4xl mx-auto">
-      <div className="flex items-center justify-between mb-6 lg:mb-20">
+    <div className="flex flex-col justify-evenly sm:justify-between h-full w-full p-3 mx-auto max-w-full sm:max-w-4xl">
+      <div className="flex flex-col sm:flex-row items-center justify-between mb-6 sm:mb-20">
         <Image
-          priority
-          className="hidden lg:block rounded-full bg-blur"
+          className="rounded-full bg-blur w-52 sm:w-72"
           alt="Rafael Pelle picture"
           src="/images/profile.jpg"
           width={300}
           height={300}
+          priority
         />
-        <div className="container ml-5">
-          <h1 className="text-5xl">
-            <p className="whitespace-nowrap">
-              {"Rafael".split("").map((letter, index) => (
-                <AnimatedLetter key={index} letter={letter} />
-              ))}
-            </p>
-            <p className="text-8xl whitespace-nowrap">
-              {"Pelle".split("").map((letter, index) => (
-                <AnimatedLetter key={index} letter={letter} />
-              ))}
-            </p>
+        <div className="container flex flex-col items-center sm:items-start mt-5 sm:ml-5">
+          <h1 className="hidden sm:block text-5xl">
+            <p className="whitespace-nowrap">{firstName}</p>
+            <p className="text-8xl whitespace-nowrap">{lastName}</p>
           </h1>
-          <p className="text-slate-500 pl-2">
+          <h1 className="sm:hidden text-2xl text-center">
+            {firstName} {lastName}
+          </h1>
+          <p className="text-slate-500 sm:pl-2 text-center sm:text-left w-3/4 sm:w-full">
             Front-end web developer specialized in React
           </p>
         </div>
       </div>
 
-      <div className="w-12/12 lg:w-6/12">
+      <div className="w-12/12 sm:w-6/12">
         {content.map((message, index) => (
           <ChatMessage key={index} {...message} />
         ))}
