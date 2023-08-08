5a9d0527900df84641afd2570102be0b72ee5409
Rafael Pelle <rafapelle@gmail.com>
2023-08-08 02:35:48 -0300

    feat: ShowProjects content

--- src/components/ShowProjectsResponse/index.tsx
+++ src/components/ShowProjectsResponse/index.tsx
@@ -1,5 +1,19 @@
+import Link from 'next/link';
 import { Fragment } from 'react';
 
 export default function ShowProjectsResponse() {
-  return <Fragment>Some content</Fragment>;
+  return (
+    <Fragment>
+      Honestly, there&apos;s not much to show... yet...just some study projects,
+      because most of my work experience was for companies developing private
+      projects. I can show you the{' '}
+      <Link className="text-primary" target="_blank" href="https://ufsc.br/">
+        AMARO e-commerce
+      </Link>{' '}
+      and you will find my latest study projects on my{' '}
+      <Link className="text-primary" target="_blank" href="/projects">
+        Projects page.
+      </Link>
+    </Fragment>
+  );
 }
