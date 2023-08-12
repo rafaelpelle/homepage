fd168856a4ad558203560bf720d6a9fb3dd67ab5
Rafael Pelle <rafapelle@gmail.com>
2023-08-12 02:01:44 -0300

    feat: i18n toast

---
+++ src/components/I18nToast/index.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { CloseIcon } from '@/components';
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+
+export default function I18nToast() {
+  const [shouldRender, setShouldRender] = useState<boolean>(false);
+
+  useEffect(() => {
+    const hasClosedToast = localStorage.getItem('hasClosedToast');
+    setTimeout(() => {
+      setShouldRender(!Boolean(hasClosedToast));
+    }, 2000);
+  }, []);
+
+  return (
+    shouldRender && (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="absolute toast toast-top toast-start sm:toast-end mt-10 z-50"
+      >
+        <div className="alert alert-info flex flex-col">
+          <button
+            className="absolute right-5 top-5 ml-auto"
+            onClick={() => {
+              setShouldRender(false);
+              localStorage.setItem('hasClosedToast', 'true');
+            }}
+          >
+            <CloseIcon />
+          </button>
+          <p className="w-32 whitespace-break-spaces">
+            First time? You can change the language here!
+          </p>
+          -------------
+          <p className="w-32 whitespace-break-spaces">
+            Primeira vez? Você pode trocar o idioma aqui!
+          </p>
+        </div>
+      </motion.div>
+    )
+  );
+}
